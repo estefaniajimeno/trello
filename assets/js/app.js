a8954320e97df492cec6145a8ed675420ed86992
estefaniajimeno <araosjimeno@gmail.com>
2018-01-03 00:03:47 -0300

    mas arreglos

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,85 +1,59 @@
+//crear, dar atributos y clases a un imput
+//crear, dar atributos y clases a un boton
+//crear un div contenedor de nuestras tareas
+//crear un titulo donde guardaremos no que ingrese el usuario
+//crear un <a> que dija añadir tarjeta
+//al dar click en a crear un textarea
 
-/*formulario */
-document.getElementById("schedule").onclick =fuction(){
-	document.getElementById("schedule").className= "hide";
-	document.getElementById("draft").className="show";
-	document.getElementById("list").focus(); 
-}
+var mainBtn = document.getElementById("mainBtn");
 
-/* ocultar formulario */
+var container = document.getElementById("container");
 
-document.getElementById("period").onclick()=function(){
-	document.getElementById("draft").className="hide";
-	document.getElementById("schedule").className="show";
-}
+mainBtn.addEventListener("click", function(){
+	var input = document.createElement("input");
+	input.setAttribute("type", "text");
+	input.setAttribute("placeholder", "añadir tarea");
+	input.classList.add("class", "input_principal");
+	input.value;
 
-/*agregar texto*/
-document.getElementById("makeList").onclick=fuction(){
-	var list=document.getElementById("list").value;
-	document.getElementById("list").value="";
-	var newList=document.createElement("div");
-	var secondSection=document.getElementById("secondSection");
-	var paragraph=document.createElement("p");
-	var nodoText=document.createTextNode("list");
-	var add = document.createElement('a');
-	add.setAtribute('href','#');
-	var text Add=document.createTextNode('añadir tarjeta')
-}
-	paragraph.appenChild(nodoText);
-	newList.appenChild(paragraph);
-	newList.className="section";
-	add.appenChild(textAdd);
-	add.className="card";
-	newList.appenChild(add);
-	/*añade un elemento antes de otro elemento*/
-	secondSection.insertBefore(newList,secondSection.firstChild);
+	var btn = document.createElement("button");
+	btn.setAttribute("id", "guardar");
+	var text_btn = document.createTextNode("Guardar");
 
-	/*text area*/
-	add.addEventListener('click',function(){
-		var textarea=document.createElement('textarea');
-		var button= document.createElement('button');
-		var add1= document.createElement('a');
-		add1.setAtribute('href','#');
-		var textAdd =document.createTextNode('añadir');
-		var time =document.createElement('a')
-		time.setAtribute('href'.'#');
-		var times=document.createElement('i');//para cancelar
-		var times.classList.add("fa","fa-times,times");
-			/*Añadiendo*/
+	container.appendChild(input);
+	btn.appendChild(text_btn);
+	container.appendChild(btn);
+
+	var btn_save = document.getElementById("guardar");
+	btn_save.addEventListener("click", function(){
+	var container_user = document.createElement("div");
+	var title_user = document.createElement("h5");
+	var text_title_user = document.createTextNode(input.value);
+	title_user.appendChild(text_title_user);
+	container_user.appendChild(title_user);
+	container.appendChild(container_user);
+
+		//cree mi <a> para luego darle atributos
+	var link = document.createElement('a');
+	link.setAttribute("href", "#");
+	var text_link = document.createTextNode('Añade una tarjeta....');
+	link.appendChild(text_link);
+	container_user.appendChild(link);
+
+	link.addEventListener("click", function(){
+	var container_list_task = document.createElement("div");
+	var textarea = document.createElement("input");
+	textarea.setAttribute("type", "text");
+	container_list_task.appendChild(textarea);
+	container_user.appendChild(container_list_task);
+
+	var btn_save_list_task = document.createElement("button");
+	var text_btn_save_list_task = document.createTextNode("Guardar Tareas");
+	btn_save_list_task.appendChild(text_btn_save_list_task);
+	container_list_task.appendChild(btn_save_list_task);
+		})
 	})
-	button.appenChild(add1)
-		add1.appenChild(textAdd);
-		newList.appenChild(textarea);
-		newList.appenChild(button)
-		newList.appenChild(time);
-		time.appenChild(time);
-		button.className="add";
-		textarea.className="text";
-		//ocultar btn de agregar tarjeta
-		add.className = "hide";
-		//se remueve el text area cuando no se ocupe;
-
-		time.addEventListener("click",function(){
-		var nodes = newList.childNodes;
-		var nodeChange.remove();
-	})
-		//añadir hacia abajo
-
-		add1.addEventListener('click',function(){
-			//childnodes selecciona todos los hijos de newList
-			var nodeChange = newList.childNodes;
-			//obtenemos el antepenultimo nodo que corresponde al elemento que queremos cambiar 
-			// el último es el icono, el penultimo es el boton y el ante penultimo el text area que estamos creando
-			var nodeChange  = nodes[nodes.length - 3];
-			//se cambia la clase al textarea que se agrega
-			nodeChange.className="text2";
-			nodeChange.disabled= "true";
-			var textarea =document.createElement('textarea');
-
-			textarea.className = "text";
-
-			newList.insertBefore(textarea,button);
-
-		}
+	
+})
 		
 	
